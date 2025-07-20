f63d9ebcb2d6a3edcbe981516dcff2dbe3ac4bde
BluPearl-J <ogheneokeno@gmail.com>
2025-07-20 01:21:03 +0100

    Create Exercise 2.27 CheckerBoardPattern.java

---
+++ Exercise 2.27 CheckerBoardPattern.java
@@ -0,0 +1,18 @@
+public class CheckerBoardPattern{
+
+public static void main (String[]args){
+
+String a= """
+                * * * * * * * *
+                * * * * * * * *
+                * * * * * * * *
+                * * * * * * * *
+                * * * * * * * *
+                * * * * * * * *
+                * * * * * * * *
+                * * * * * * * *  """ ;
+
+System.out.println (a);
+
+}
+}
