8ae106bdfbe9bd777fc9361f1318d1f26f790444
BluPearl-J <ogheneokeno@gmail.com>
2025-07-20 01:01:01 +0100

    Create Exercise 2.18 Shapes.java

---
+++ Exercise 2.18 Shapes.java
@@ -0,0 +1,19 @@
+public class Shapes{
+
+public static void main (String[]args){
+
+String s= """
+              *********      ***      *       *
+              *       *     *   *    ***     * *
+              *       *    *     *   *****  *    *
+              *       *   *       *   *    *      *
+              *       *   *        *   *   *      *
+              *       *   *        *   *    *     *
+              *       *   *        *   *     *   *
+              *       *   *        *   *       *
+              *********     * * * *                 """ ;
+
+System.out.println (s);
+
+}
+}
