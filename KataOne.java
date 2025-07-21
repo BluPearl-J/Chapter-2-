039137008152b6fad00d72d11e5bd254ba3c266c
BluPearl-J <ogheneokeno@gmail.com>
2025-07-21 02:45:54 +0100

    Update KataOne.java

--- KataOne.java
+++ KataOne.java
@@ -20,3 +20,25 @@ System.out.println(KataOne.isPrime(9));
 }
 
 
+
+
+
+MinusSubtract.java
+
+public class MinusSubtract {
+
+
+public static int difference(int i1, int i2) {
+int result = i2-i1;
+
+
+return result;
+}
+
+public static void main(String[] args) {
+System.out.println("Difference of 1 and 10 is " + difference(1, 10));
+System.out.println("Difference of20 and 37 is " + difference(20, 37));
+System.out.println("Difference of35 and 49 is " + difference(35, 49));
+}
+}
+
