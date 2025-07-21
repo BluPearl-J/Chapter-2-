c9299697d10b4d67320bee03d8d91548f08db68b
BluPearl-J <ogheneokeno@gmail.com>
2025-07-21 02:53:00 +0100

    Update KataOne.java

--- KataOne.java
+++ KataOne.java
@@ -37,6 +37,38 @@ return result;
 
 public static void main(String[] args) {
 System.out.println("Difference of 1 and 10 is " + difference(1, 10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ public class Divide {
+
+
+public static int divide(int i1, int i2) {
+int result = i2/i1;
+
+
+return result;
+}
+public static void main(String[] args) {
+System.out.println("Division of 1 and 10 is " + divide(1, 10));
+System.out.println("Division of20 and 37 is " + divide(20, 37));
+System.out.println("Division of35 and 49 is " + divide(35, 49));
+}
+}
 System.out.println("Difference of20 and 37 is " + difference(20, 37));
 System.out.println("Difference of35 and 49 is " + difference(35, 49));
 }
