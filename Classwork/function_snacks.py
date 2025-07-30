1bc773cb361fe88faa896d5ed2730b253e91094d
BluPearl-J <ogheneokeno@gmail.com>
2025-07-30 12:07:58 +0100

    Update function_snacks.py
    create function so that if nos divisible by 5, retrurnts its square root
    else return remainder of the number divided by 5.
    handle edge cases like negative nos and non negative inouts .
    input 10 function returns 3.16 AS SQUARE ROOT

--- Classwork/function_snacks.py
+++ Classwork/function_snacks.py
@@ -17,4 +17,9 @@ print(get_divisor(19))
 
 
 
+#create function so that if nos divisible by 5, retrurnts its square root
+#else return remainder of the number divided by 5.
+#handle edge cases like negative nos and non negative inouts .
+#input 10 function returns 3.16 AS SQUARE ROOT
+
 
