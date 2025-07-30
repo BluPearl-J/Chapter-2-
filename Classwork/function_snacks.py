527a491230518df0317727ab207c2681426703ff
BluPearl-J <ogheneokeno@gmail.com>
2025-07-30 12:05:17 +0100

    Add files via upload

---
+++ Classwork/function_snacks.py
@@ -0,0 +1,20 @@
+def get_divisor(number):
+
+   if number%5==0:
+         return number**0.5
+   else:
+         return  number%5
+   
+print(get_divisor(10))
+
+print(get_divisor(12))
+
+print(get_divisor(13))
+
+print(get_divisor(19))
+
+
+
+
+
+
