e2a3495dfe70e88d1fd2fb52c24bb0b75042d76f
BluPearl-J <ogheneokeno@gmail.com>
2025-08-05 11:09:04 +0100

    Update 5th august list.py

--- Classwork/5th august list.py
+++ Classwork/5th august list.py
@@ -8,3 +8,6 @@ for index in range(-11,0):
 new_list=["Makaveli", "Chiamaka", "Basit", "Fareed"]
 for name in new_list:
  print(name)
+ print(new_list[-1])
+ print(new_list[-3])
+ print(new_list[-4])
