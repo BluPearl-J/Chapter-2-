28786fabff7912f31f7e363f80d08e79e11fb7f5
BluPearl-J <ogheneokeno@gmail.com>
2025-08-05 11:06:27 +0100

    Create 5th august list.py

---
+++ Classwork/5th august list.py
@@ -0,0 +1,10 @@
+for index in range(0,10):
+ print(index)
+for index in range(-11,-1):
+ print(index)
+#value left must be smaller in range functyion
+for index in range(-11,0):
+ print(index)
+new_list=["Makaveli", "Chiamaka", "Basit", "Fareed"]
+for name in new_list:
+ print(name)
