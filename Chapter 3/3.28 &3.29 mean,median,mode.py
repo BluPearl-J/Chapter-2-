2ed20bbbe8a48b89ebd9c49ba7c9645ecea96e1a
BluPearl-J <ogheneokeno@gmail.com>
2025-08-13 12:07:18 +0100

    Update 3.28 &3.29 mean,median,mode.py

--- Chapter 3/3.28 &3.29 mean,median,mode.py
+++ Chapter 3/3.28 &3.29 mean,median,mode.py
@@ -1,1 +1,13 @@
 
+#3.28 (Intro to Data Science: Mean, Median and Mode) Calculate the mean, median and
+#mode of the values 9, 11, 22, 34, 17, 22, 34, 22 and 40. Suppose the values included another 34. What9, problem might occ
+
+
+
+import statistics
+values = [9, 11, 22, 34, 17, 22, 34,22,40]
+print("The mean is:",+ statistics.mean(values))
+print("The median is",+ statistics.median(values))
+print("The mode is",+ statistics.mode(values))
+
+my_array=[ 9, 11, 22, 34, 17, 22, 34, 22, 40]
