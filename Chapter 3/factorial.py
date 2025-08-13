37e653f860940ad93275a5cf94198798dddc1823
BluPearl-J <ogheneokeno@gmail.com>
2025-08-13 13:41:16 +0100

    Create factorial.py

---
+++ Chapter 3/factorial.py
@@ -0,0 +1,17 @@
+def get_factorial(n):
+    total = 1
+    for i in range(1, n + 1):
+        total *= i
+    print("Factorial of", n, "is", total)
+
+number = 5
+hey=180
+okay=20
+number1=10
+number2=60
+
+get_factorial(number)
+get_factorial(number1)
+get_factorial(okay)
+get_factorial(number2)
+get_factorial(hey)
