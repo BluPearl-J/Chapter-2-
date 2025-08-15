a27aa85dfa425aa98e61ed7cafdd36f434b34a18
BluPearl-J <ogheneokeno@gmail.com>
2025-08-15 08:08:32 +0100

    Create Pseudocode3.21.py

---
+++ Chapter 3/Pseudocode3.21.py
@@ -0,0 +1,23 @@
+3.22 (Optional else C
+sub,mit pseudocode seoerate files
+def Calculate_change(money):
+ iny input(enter input)
+ while (input<=100cents){ do the following switch int change
+  int result=input%quarters
+  int dimesresultresult2=input-input%quarter)%dimes
+  int penniesresult3=input-result2
+ int change
+ switch match change;
+case 1
+result==0
+print your change isx quarters
+case2
+if result1!=0 andand result3==0
+print  your change is x quarters y dime
+case 3
+if result1!=0 and result3!=0
+print your change is x quarters y dime x pennies
+example
+calcyulate-change 75
+
+ print("your change is",change)
