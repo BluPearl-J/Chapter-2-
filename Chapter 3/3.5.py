4adae532614b3cecc3664ad7ee306326eeb7cf98
BluPearl-J <ogheneokeno@gmail.com>
2025-08-15 09:19:18 +0100

    Create 3.5.py

---
+++ Chapter 3/3.5.py
@@ -0,0 +1,2 @@
+#3.5 (if…else Statements) Reimplement the script of Fig. 2.1 using three if…else
+#statements rather than six if statements. [Hint: For example, think of == and != as “opposite” tests.]
